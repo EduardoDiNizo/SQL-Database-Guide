ab7c4e10c9b35783eacc5225e5984fdec7ec5844
Eduardo <eduardonizo@as2group.com.br>
2024-05-23 14:37:46 -0300

    Added basic files

---
+++ 1-Quick Start/README.md
@@ -0,0 +1,22 @@
+## Quick Start
+
+#### MySQL é um sistema de gerenciamento de banco de dados relacional (RDBMS) de código aberto amplamente utilizado. Ele fornece um ambiente robusto e confiável para armazenar e gerenciar dados, oferecendo suporte a recursos como transações ACID, índices, chaves estrangeiras e muito mais. MySQL é conhecido por sua velocidade, escalabilidade e facilidade de uso, tornando-o uma escolha popular para aplicativos da web e outras aplicações que exigem armazenamento e recuperação eficientes de dados.
+
+#### Este arquivo docker-compose.yml define um serviço MySQL usando Docker Compose. O serviço é configurado com as seguintes características:
+
+
+- **Imagem**: Utiliza a imagem mais recente do MySQL disponível no Docker Hub.
+
+- **0ome do Contêiner**: O contêiner será chamado de mysql.
+- **Reinicialização Automática**: Configurado para reiniciar sempre que o Docker reiniciar.
+- **Variáveis de Ambiente**:
+  - **MYSQL_DATABASE**: Define o nome do banco de dados como store.
+  - **MYSQL_USER**: Define o usuário do banco de dados como store_admin.
+  - **MYSQL_PASSWORD**: Define a senha do usuário do banco de dados como !StringP@ssw@rd.
+  - **MYSQL_ROOT_PASSWORD**: Define a senha do usuário root do MySQL como !StringP@ssw@rd.
+
+- **Portas**: Mapeia a porta 3306 do contêiner MySQL para a porta 3306 do host, permitindo acesso ao banco de dados.
+
+- **Volumes**: Monta um volume chamado mysql_data para armazenar os dados do banco de dados MySQL de forma persistente no host.
+MySQL
+
---
+++ 3-DDL, DML, DCL, TCL/dml.sql
@@ -0,0 +1,55 @@
+-- Insert an address
+INSERT INTO address (id, street, city, state, country) VALUES
+('1f78f1e4-5b7a-4c20-8cf6-2a68329d1fc9', 'Street A', 'City A', 'State A', 'Country A');
+
+-- Insert a user profile
+INSERT INTO user_profile (id, user_id, birth_date, gender) VALUES
+('2d688edf-17f1-4f3e-bf9a-1fc8b2c73a06', '1f78f1e4-5b7a-4c20-8cf6-2a68329d1fc9', '1990-01-01', 'Male');
+
+-- Insert a user
+INSERT INTO user (id, name, email, profile_id, address_id) VALUES
+('1f78f1e4-5b7a-4c20-8cf6-2a68329d1fc9', 'John', 'john@example.com', '2d688edf-17f1-4f3e-bf9a-1fc8b2c73a06', '1f78f1e4-5b7a-4c20-8cf6-2a68329d1fc9');
+
+-- Insert a product category
+INSERT INTO product_category (id, name, description) VALUES
+('d4c4f26e-9f4d-42c8-8ff2-03f05fd44c20', 'Category A', 'Category A description');
+
+-- Insert a product
+INSERT INTO product (id, name, description, price, category_id) VALUES
+('a12f5b5a-f6a6-4d7f-8042-bd8b17f3f4b9', 'Product A', 'Product A description', 10.50, 'd4c4f26e-9f4d-42c8-8ff2-03f05fd44c20');
+
+-- Insert a payment method
+INSERT INTO payment_method (id, name, description) VALUES
+('e7e32561-ec3a-4c67-80b8-070c93407c6f', 'Credit Card', 'Credit Card payment');
+
+-- Insert an order
+INSERT INTO order (id, user_id, payment_method_id, status, order_date) VALUES
+('b0b18b8f-0680-4548-9fcf-6e96fd8d73b3', '1f78f1e4-5b7a-4c20-8cf6-2a68329d1fc9', 'e7e32561-ec3a-4c67-80b8-070c93407c6f', 'In progress', '2024-03-12');
+
+-- Insert an order item
+INSERT INTO order_item (id, order_id, product_id, quantity, unit_price) VALUES
+('5faea477-8f08-4779-89cb-46f69b1d164d', 'b0b18b8f-0680-4548-9fcf-6e96fd8d73b3', 'a12f5b5a-f6a6-4d7f-8042-bd8b17f3f4b9', 2, 10.50);
+
+
+-- INNER JOIN para obter o nome do usuário e o nome da categoria do produto
+SELECT u.name AS user_name, p.name AS product_name, pc.name AS category_name
+FROM `order` o
+INNER JOIN user u ON o.user_id = u.id
+INNER JOIN order_item oi ON o.id = oi.order_id
+INNER JOIN product p ON oi.product_id = p.id
+INNER JOIN product_category pc ON p.category_id = pc.id;
+
+-- LEFT JOIN para obter todos os usuários, mesmo os que não fizeram pedidos
+SELECT u.name AS user_name, o.id AS order_id
+FROM user u
+LEFT JOIN `order` o ON u.id = o.user_id;
+
+-- RIGHT JOIN para obter todos os pedidos, mesmo os que não têm um usuário associado
+SELECT u.name AS user_name, o.id AS order_id
+FROM `order` o
+RIGHT JOIN user u ON o.user_id = u.id;
+
+-- FULL JOIN para obter todos os usuários e todos os pedidos, combinando quando possível
+SELECT u.name AS user_name, o.id AS order_id
+FROM user u
+FULL JOIN `order` o ON u.id = o.user_id;
